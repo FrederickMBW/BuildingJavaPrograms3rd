2046946d6f6ef258fbf99629a0bb1816bae09035
Frederick Wirtz <frederickwirtz@corp.sanmar.com>
2020-01-15 12:24:17 -0800

    Completed Chapter 01 Exercise 05

---
+++ Chapter01/Exercises/Exercise05/MuchBetter.java
@@ -0,0 +1,10 @@
+public class MuchBetter {
+    public static void main(String[] args) {
+        System.out.println("A \"quoted\" String is");
+        System.out.println("'much' better if you learn");
+        System.out.println("the rules of the \"escape sequences.\"");
+        System.out.println("Also, \"\" represents an empty String.");
+        System.out.println("Don't forget: use \\\" instead of \" !");
+        System.out.println("'' is not the same as \"");
+    }
+}
