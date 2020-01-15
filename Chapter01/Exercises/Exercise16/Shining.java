80b39227cbc80759d1e3fc395290a9f6cea85d55
Frederick Wirtz <frederickwirtz@corp.sanmar.com>
2020-01-15 13:37:48 -0800

    Completed Chapter 01 Exercise 16

---
+++ Chapter01/Exercises/Exercise16/Shining.java
@@ -0,0 +1,44 @@
+public class Shining {
+    public static void main(String[] args) {
+        print1000();
+    }
+
+    public static void print1000() {
+        print100();
+        print100();
+        print100();
+        print100();
+        print100();
+        print100();
+        print100();
+        print100();
+        print100();
+        print100();
+    }
+
+    public static void print100() {
+        print10();
+        print10();
+        print10();
+        print10();
+        print10();
+        print10();
+        print10();
+        print10();
+        print10();
+        print10();
+    }
+
+    public static void print10() {
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+    }
+}
