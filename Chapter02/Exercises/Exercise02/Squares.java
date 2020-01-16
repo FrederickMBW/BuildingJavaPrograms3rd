f09ee4c6f1384b0b205e6fe1426ac7648267457f
Frederick Wirtz <frederickwirtz@corp.sanmar.com>
2020-01-16 14:42:09 -0800

    Fixed Chapter 02 Exercise 02 solution to only print on one line

--- Chapter02/Exercises/Exercise02/Squares.java
+++ Chapter02/Exercises/Exercise02/Squares.java
@@ -1,7 +1,7 @@
 public class Squares {
     public static void main(String[] args) {
         for(int i = 1; i <= 10; i++) {
-            System.out.println(i * i + " ");
+            System.out.print(i * i + " ");
         }
     }
 }
