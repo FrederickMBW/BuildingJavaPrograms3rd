7aba4b25299587919c40d584ed2695e48379ab36
Frederick Wirtz <frederickwirtz@Fredericks-MacBook-Pro.local>
2020-01-16 18:31:49 -0800

    Completed Chapter 02 Exercise 20

--- Chapter02/Exercises/Exercise20/StarFigure.java
+++ Chapter02/Exercises/Exercise20/StarFigure.java
@@ -2,6 +2,23 @@ public class StarFigure {
     public static final int HEIGHT = 5;
 
     public static void main(String[] args) {
+        for (int i = 0; i < HEIGHT; i++) {
+            // Draw left side
+            for (int j = 0; j < -i + (HEIGHT - 1); j++) {
+                System.out.print("////");
+            }
 
+            // Draw the inside
+            for (int j = 0; j < i; j++) {
+                System.out.print("********");
+            }
+
+            // Draw right side
+            for (int j = 0; j < -i + (HEIGHT - 1); j++) {
+                System.out.print("\\\\\\\\");
+            }
+
+            System.out.println();
+        }
     }
 }
