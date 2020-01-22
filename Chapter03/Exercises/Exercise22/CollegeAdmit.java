f208da148e90a37d663f5aed5a7792f0e6146689
Frederick Wirtz <frederickwirtz@corp.sanmar.com>
2020-01-22 13:44:26 -0800

    Started Chapter 03 Exercise 22. Changing computers

---
+++ Chapter03/Exercises/Exercise22/CollegeAdmit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CollegeAdmit {
+    public static void main(String[] args) {
+        System.out.println("University admission Program!");
+        Scanner console = new Scanner(System.in);
+        System.out.print("What is your GPA? ");
+        double gpa = console.nextDouble();
+        System.out.print("What is your SAT score? ");
+        int sat = console.nextInt();
+
+        
+    }
+}
