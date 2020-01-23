ee0efce048994fc7f087b53b8f3aed4360b9d8e7
Frederick Wirtz <frederickwirtz@Fredericks-MacBook-Pro.local>
2020-01-22 17:22:07 -0800

    Completed Chapter 03 Exercise 22. Required creativity to only used what has been learned up to this point in the book

--- Chapter03/Exercises/Exercise22/CollegeAdmit.java
+++ Chapter03/Exercises/Exercise22/CollegeAdmit.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class CollegeAdmit {
     public static void main(String[] args) {
@@ -8,7 +9,41 @@ public class CollegeAdmit {
         double gpa = console.nextDouble();
         System.out.print("What is your SAT score? ");
         int sat = console.nextInt();
+        admissionMessage(sat, gpa);
+    }
 
-        
+    public static void admissionMessage(int sat, double gpa) {
+        // Declare and initialize the minimum allowed scores
+        int minSAT = 900;
+        int minGPA = 18;
+
+        // Convert GPA to a integer
+        int convertedGPA = (int) Math.floor(gpa * 10);
+
+        // Remove the min SAT score required from the actual SAT score
+        for (int i = 0; i < minSAT; i++) {
+            sat--;
+        }
+
+        // Remove the min GPA score required from the actual GPA score
+        for (int i = 0; i < minGPA; i++) {
+            convertedGPA--;
+        }
+
+        // If the modified SAT score is below zero, print to the console not accepted
+        for (int i = sat; i < 0; i++) {
+            System.out.println("You are NOT accepted");
+            return;
+        }
+
+        // If the modified GPA score is below zero, print the the console not
+        // accepted
+        for (int i = convertedGPA; i < 0; i++) {
+            System.out.println("You are NOT Accepted");
+            return;
+        }
+
+        // Print the the console accepted
+        System.out.println("You were accepted!");
     }
 }
