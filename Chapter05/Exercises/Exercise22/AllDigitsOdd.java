d539a17da4e25d1606dbb6174383c5e6a83da0ed
Frederick Wirtz <frederickwirtz@Fredericks-MacBook-Pro.local>
2020-02-03 19:44:58 -0800

    Fixed Chapter 05 Exercise 22 to work with negatives

--- Chapter05/Exercises/Exercise22/AllDigitsOdd.java
+++ Chapter05/Exercises/Exercise22/AllDigitsOdd.java
@@ -7,7 +7,7 @@ public class AllDigitsOdd {
 
     // Returns true if all digits are odd in an integer
     public static boolean allDigitsOdd(int n) {
-        while (n > 0) {
+        while (n != 0) {
             if (n % 2 == 0) {
                 return false;
             }
