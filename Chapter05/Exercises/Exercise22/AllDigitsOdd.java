ee6dc49f9309b68034f93647de51826231240e53
Frederick Wirtz <frederickwirtz@Fredericks-MacBook-Pro.local>
2020-02-03 19:37:57 -0800

    Completed Chapter 05 Exercise 22

---
+++ Chapter05/Exercises/Exercise22/AllDigitsOdd.java
@@ -0,0 +1,19 @@
+public class AllDigitsOdd {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+
+        System.out.println(allDigitsOdd(n));
+    }
+
+    // Returns true if all digits are odd in an integer
+    public static boolean allDigitsOdd(int n) {
+        while (n > 0) {
+            if (n % 2 == 0) {
+                return false;
+            }
+            n /= 10;
+        }
+
+        return true;
+    }
+}
