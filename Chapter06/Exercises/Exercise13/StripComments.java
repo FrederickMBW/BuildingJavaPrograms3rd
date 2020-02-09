8af6a70459e1e152e9a92dad937bef3b8a585b41
Frederick Wirtz <frederickwirtz@Fredericks-MacBook-Pro.local>
2020-02-08 21:20:13 -0800

    Completed Chapter 06 Exercise 13. Does not work for all edge cases

---
+++ Chapter06/Exercises/Exercise13/StripComments.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class StripComments {
+    public static void main(String[] args) throws FileNotFoundException {
+        // Get a file name from the command line and use it to create a scanner
+        Scanner input = new Scanner(new File(args[0]));
+
+        // Print the file with the comments removed
+        stripComments(input);
+    }
+
+    // Strip the comments from a file and print to the console
+    // I assume a // comment is never inside of a /* */ comment
+    public static void stripComments(Scanner input) {
+        while (input.hasNextLine()) {
+            String line = input.nextLine();
+
+            while (line.contains("/*")) {
+                System.out.print(line.substring(0, line.indexOf("/*")));
+
+                while (!line.contains("*/") && input.hasNextLine()) {
+                    line = input.nextLine();
+                }
+
+                line = line.substring(line.indexOf("*/") + 2);
+            }
+
+            if (line.contains("//")) {
+                System.out.println(line.substring(0, line.indexOf("//")));
+            } else {
+                System.out.println(line);
+            }
+        }
+    }
+}
---
+++ Chapter06/Exercises/Exercise13/program.java
@@ -0,0 +1,13 @@
+import java.util.* 
+
+/* My Program
+by Suzu Student */
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // a println
+    }
+
+    public static /* Hello there */ void foo() {
+        System.out.println("Goodbye!"); // comment here
+    } /* */
+}
