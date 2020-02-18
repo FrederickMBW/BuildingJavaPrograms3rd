aaa435f8f26e57e2f120acc720eaa12a73ee75a7
Frederick Wirtz <frederickwirtz@corp.sanmar.com>
2020-02-17 18:44:13 -0800

    Completed Chapter 08 Exercise 20

--- Chapter08/Exercises/Exercise18/Rectangle.java
+++ Chapter08/Exercises/Exercise18/Rectangle.java
@@ -37,4 +37,12 @@ public class Rectangle {
     public String toString() {
         return String.format("Rectangle[x=%d,y=%d,width=%d,height=%d]", x, y, width, height);
     }
+
+    public boolean contains(int x, int y) {
+        return x >= this.x && x <= this.x + width && y >= this.y && y <= this.y + height;
+    }
+
+    public boolean contains(Point p) {
+        return contains(p.x, p.y);
+    }
 }
