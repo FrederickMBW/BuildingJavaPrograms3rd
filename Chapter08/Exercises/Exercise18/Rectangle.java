e7b2686b91c194425775ec2ffc2cde76f412e9fb
Frederick Wirtz <frederickwirtz@corp.sanmar.com>
2020-02-17 18:40:30 -0800

    Completed Chapter 08 Exercise 19

---
+++ Chapter08/Exercises/Exercise18/Rectangle.java
@@ -0,0 +1,40 @@
+import java.awt.Point;
+
+public class Rectangle {
+    private int x, y, width, height;
+
+    public Rectangle(Point p, int width, int height) {
+        this(p.x, p.y, width, height);
+    }
+
+    public Rectangle(int x, int y, int width, int height) {
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String toString() {
+        return String.format("Rectangle[x=%d,y=%d,width=%d,height=%d]", x, y, width, height);
+    }
+}
