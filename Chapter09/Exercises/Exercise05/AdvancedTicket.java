aa6d5afa2d9d418a32c0e920b7a2a1073fbe28f9
Frederick Wirtz <frederickwirtz@corp.sanmar.com>
2020-02-19 12:31:53 -0800

    Completed Chapter 09 Exercise 07

---
+++ Chapter09/Exercises/Exercise05/AdvancedTicket.java
@@ -0,0 +1,11 @@
+public class AdvancedTicket extends Ticket {
+    public AdvancedTicket(int number, int numberOfDaysBefore) {
+        super(number);
+        
+        if (numberOfDaysBefore >= 10) {
+            super.setPrice(30.0);
+        } else {
+            super.setPrice(40.0);
+        }
+    }
+}
