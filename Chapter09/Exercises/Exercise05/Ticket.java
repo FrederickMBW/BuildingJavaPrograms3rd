26313d06e7df95ede139d4bd9d01d66265d3be65
Frederick Wirtz <frederickwirtz@corp.sanmar.com>
2020-02-19 12:29:47 -0800

    Completed Chapter 09 Exercise 06

--- Chapter09/Exercises/Exercise05/Ticket.java
+++ Chapter09/Exercises/Exercise05/Ticket.java
@@ -1,6 +1,10 @@
 public abstract class Ticket {
     int number;
-    double price;
+    double price;\
+
+    public Ticket() {
+        this.number = 0;
+    }
 
     public Ticket(int number) {
         this.number = number;
---
+++ Chapter09/Exercises/Exercise05/WalkupTicket.java
@@ -0,0 +1,6 @@
+public class WalkupTicket extends Ticket {
+    public WalkupTicket(int number) {
+        super(number);
+        super.setPrice(50.0);
+    }
+}
