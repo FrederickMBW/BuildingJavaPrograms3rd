2ae0671fc2942ece879cc95f84149e58f48cb54d
Frederick Wirtz <frederickwirtz@corp.sanmar.com>
2020-02-24 13:44:38 -0800

    Improved Chapter 11 Exercise 03

--- Chapter11/Exercises/Exercise03/RemoveInRange.java
+++ Chapter11/Exercises/Exercise03/RemoveInRange.java
@@ -1,10 +1,20 @@
 import java.util.*;
 
 public class RemoveInRange {
-    public static void removeInRange(LinkedList<Integer> list, int value, int start, int end) {
-        for (int i = end - 1; i >= start; i--) {
-            if (list.get(i) == value) {
-                list.remove(i);
+    public static void removeInRange(List<Integer> list, int value, int start, int end) {
+        // Create an iterator to go through the list
+        Iterator<Integer> itr = list.iterator();
+
+        // Move the iterator to the start position
+        for (int i = 0; i < start; i++) {
+            itr.next();
+        }
+
+        // Go through and remove any elements equal to the value between
+        // the starting and ending index
+        for (int i = 0; i < end - start; i++) {
+            if (itr.next() == value) {
+                itr.remove();
             }
         }
     }
