46231c9e2397d614f9b86acb4ea1492b860f55cf
Frederick Wirtz <frederickwirtz@Fredericks-MacBook-Pro.local>
2020-02-23 19:56:49 -0800

    Completed Chapter 10 Exercise 12

---
+++ Chapter10/Exercises/Exercise12/MarkLength.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class MarkLength {
+    public static void markLength(ArrayList<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).length() == 4) {
+                list.add(i, "****");
+                i++;
+            }
+        }
+    }
+}
