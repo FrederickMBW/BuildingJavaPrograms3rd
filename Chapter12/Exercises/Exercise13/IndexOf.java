36d71818b87445f88871f8bdffbb8bff6e1c1d02
Frederick Wirtz <frederickwirtz@corp.sanmar.com>
2020-03-02 17:33:18 -0800

    Completed Chapter 12 Exercise 13

---
+++ Chapter12/Exercises/Exercise13/IndexOf.java
@@ -0,0 +1,30 @@
+public class IndexOf {
+    public static void main(String[] args) {
+        String s = args[0];
+        String sub = args[1];
+
+        System.out.println(indexOf(s, sub));
+    }
+
+    public static int indexOf(String s, String sub) {
+        return indexOf(s, sub, 0);
+    }
+
+    private static int indexOf(String s, String sub, int index) {
+        if (sub.length() + index > s.length()) {
+            return -1;
+        }
+
+        for (int i = 0; i < sub.length(); i++) {
+            if (i >= s.length() - 1) {
+                return -1;
+            }
+
+            if (s.charAt(index + i) != sub.charAt(i)) {
+                return indexOf(s, sub, index + 1);
+            }
+        }
+
+        return index;
+    }
+}
